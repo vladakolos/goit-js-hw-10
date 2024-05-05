95c6975289dac508ddf19503e3cce7aaad35b613
vladakolos <linikvladislava@gmail.com>
2024-05-05 16:09:56 +0300

    2-task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,62 +1,38 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+const form = document.querySelector('.form');
 const delayInput = document.querySelector('input[name="delay"]');
-const fulfilledBtn = document.querySelector(
-  'input[name="state"][value="fulfilled"]'
-);
-const rejectedBtn = document.querySelector(
-  'input[name="state"][value="rejected"]'
-);
-const submitBtn = document.querySelector('button[type="submit"]');
 
-const getFulfilledPromise = delay => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(delay);
-    }, delay);
-  });
-};
-
-const getRejectedPromise = delay => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(delay);
-    }, delay);
-  });
-};
-
-submitBtn.addEventListener('click', event => {
+form.addEventListener('submit', event => {
   event.preventDefault();
-  const delay = parseInt(delayInput.value);
 
-  if (fulfilledBtn.checked) {
-    getFulfilledPromise(delay)
-      .then(delay => {
-        iziToast.success({
-          title: `✅`,
-          message: `Fulfilled promise in ${delay} ms`,
-        });
-      })
-      .catch(delay => {
-        iziToast.error({
-          title: `❌`,
-          message: `Rejected promise in ${delay} ms`,
-        });
+  const delay = parseInt(delayInput.value);
+  const radioBtns = form.elements.state.value;
+
+  const getPromise = delay => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (radioBtns === 'fulfilled') {
+          resolve(`Fulfilled promise in ${delay}ms`);
+        } else {
+          reject(` Rejected promise in ${delay}ms`);
+        }
+      }, delay);
+    });
+  };
+
+  getPromise(delay)
+    .then(value => {
+      iziToast.success({
+        title: `✅`,
+        message: value,
       });
-  } else if (rejectedBtn.checked) {
-    getRejectedPromise(delay)
-      .then(delay => {
-        iziToast.success({
-          title: `✅`,
-          message: `Fulfilled promise in ${delay} ms`,
-        });
-      })
-      .catch(delay => {
-        iziToast.error({
-          title: `❌`,
-          message: `Rejected promise in ${delay} ms`,
-        });
+    })
+    .catch(error => {
+      iziToast.error({
+        title: `❌`,
+        message: error,
       });
-  }
+    });
 });
